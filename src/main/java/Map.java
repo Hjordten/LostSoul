c1e300b7f278b3d2a44c024680db2758aace2003
Hjordten <andr314e@stud.kea.dk>
2022-10-16 13:27:14 +0200

    Created instances of room, not fully filled with information

---
+++ src/main/java/Map.java
@@ -0,0 +1,161 @@
+public class Map {
+
+    //We weite Room because Room objects needs to attributes when creating objects.
+    //Starter room tells game which room player starts in, when Map class is building the map. This is static and.
+    //Current room will be changeable since it needs to update every time player succesfully moves.
+    private Room starterRoom;
+    private Room currentRoom;
+
+    //We create get for instances of room. It can then tell Room class where player currently is.
+    //Room can then set the correct directions player can move and use get, to get all the moveable directions.
+    private Room getStarterRoom()
+    {
+        return starterRoom;
+    }
+
+    private Room getCurrentRoom()
+    {
+        return currentRoom;
+    }
+
+    /*
+    MIGHT BE USELESS CODE
+
+    public void setCurrentRoom(Room room){
+        currentRoom = room;
+    }
+     */
+
+    public Map()
+    {
+        buildMap();
+    }
+
+
+    public void buildMap(){
+    Room room1 = new Room
+            ("Region of: Hells deep",
+            "Home",
+            "Starter room",
+            "Your own hellish house filled with emptiness",
+            false);
+    Room room2 = new Room(
+            "",
+            "",
+            "",
+            "",
+            false);
+    Room room3 = new Room(
+            "",
+            "",
+            "",
+            "",
+            false);
+    Room room4 = new Room(
+            "",
+            "",
+            "",
+            "",
+            false);
+    Room room5 = new Room(
+            "",
+            "",
+            "",
+            "",
+            false);
+    Room room6 = new Room(
+            "",
+            "",
+            "",
+            "",
+            false);
+    Room room7 = new Room(
+            "",
+            "",
+            "",
+            "",
+            false);
+    Room room8 = new Room(
+            "",
+            "",
+            "",
+            "",
+            false);
+    Room room9 = new Room(
+            "",
+            "",
+            "",
+            "",
+            false);
+    Room room10 = new Room(
+            "",
+            "",
+            "",
+            "",
+            false);
+    Room room11 = new Room(
+            "",
+            "",
+            "",
+            "",
+            false);
+    Room room12 = new Room(
+            "",
+            "",
+            "",
+            "",
+            false);
+    Room room13 = new Room(
+            "",
+            "",
+            "",
+            "",
+            false);
+    Room room14 = new Room(
+            "",
+            "",
+            "",
+            "",
+            false);
+    Room room15 = new Room(
+            "",
+            "",
+            "",
+            "",
+            false);
+    Room room16 = new Room(
+            "",
+            "",
+            "",
+            "",
+            false);
+    Room room17 = new Room(
+            "",
+            "",
+            "",
+            "",
+            false);
+    Room room18 = new Room(
+            "",
+            "",
+            "",
+            "",
+            false);
+    Room room19 = new Room(
+            "",
+            "",
+            "",
+            "",
+            false);
+    Room room20 = new Room(
+            "",
+            "",
+            "",
+            "",
+            false);
+    }
+
+
+
+
+}
--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -2,7 +2,8 @@ import java.util.ArrayList;
 
 public class Room {
 
-    //Attributes for room objects
+    //Attributes for room object
+    private String roomGameRegion;
     private String roomGameName;
     private String roomDeveloperName;
     private String roomDescription;
@@ -14,16 +15,22 @@ public class Room {
     private Room east;
     private Room west;
 
+    /*
     //Two arraylists. One for Items (pickups) and one for enemy (checks if enemy in room == true)
     private ArrayList<Item> itemsArrayList = new ArrayList<>();
     private ArrayList<Enemy> enemyArrayList = new ArrayList<>();
 
-    //Used with func to add obejcts to rooms later
+
+
+
+    //Used with func to add instances of objects to rooms later
     Item item = new Item();
+     */
 
     //defines object which we can create instances off with set attributes.
-    public Room(String roomGameName, String roomDeveloperName, String roomDescription, boolean roomDanger)
+    public Room(String roomGameRegion,String roomGameName, String roomDeveloperName, String roomDescription, boolean roomDanger)
     {
+        this.roomGameRegion = roomGameRegion;
         this.roomGameName = roomGameName;
         this.roomDeveloperName = roomDeveloperName;
         this.roomDescription = roomDescription;
@@ -32,6 +39,10 @@ public class Room {
 
 
     //Get for room attributes so it can be called outside of class.
+    public String getRoomGameRegion()
+    {
+        return roomGameRegion;
+    }
     public String getRoomGaneName(){
         return roomGameName;
     }
@@ -48,8 +59,9 @@ public class Room {
         return roomDanger;
     }
 
-    //Setter for setting directions to which rooms is connected. Is used when creating map and connects rooms to each other.
+    //Setter for setting directions to which  rooms is connected. Is used when creating map and connects rooms to each other.
     //We write Room room because Room objects is the argument to its direction.
+    //Also used when player succesfully moves and uses the set direction, to set and move the player the correct way.
     public void setEast(Room room)
     {
         this.east = east;
@@ -71,11 +83,42 @@ public class Room {
     }
 
     //get for directions in rooms
-    //We write Room because it needs to get directions, based on which room (instance of object)
+    //We write Room because it needs to get directions, based on which room (instance of object).
+    //Also used when player tries to move around, where it uses get, to get which directions is moveable.
     public Room getEast()
     {
         return east;
     }
+
+    public Room getNorth()
+    {
+        return north;
+    }
+
+    public Room getSouth()
+    {
+        return south;
+    }
+
+    public Room getWest()
+    {
+        return west;
+    }
+
+/*
+    //method to add instances of different objects to different ArrayList
+    public void addItem (Item item) //Needs to create Item class os it know where to add item.
+    {
+        itemsArrayList.add(item);
+    }
+
+    public void addEnemy(Enemy enemy) //Needs to create enemy class os it know where to add enemy.
+    {
+        enemyArrayList.add(enemy);
+    }
+
+    public void addMeleeWeapon()
+*/
 
 
 }
